6cc1924d2fe0e9ef87e1a9c7440f99d19863a041
Jennifer Massi <jennifer.massi@mscoding.org>
2022-12-29 11:54:10 -0800

    string variables

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Personal_Info
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jen";
+            string activity = " is really tired today";
+
+                Console.WriteLine(name + activity );
+
+
+        }//end main
+    }//end class
+}//end namespace
